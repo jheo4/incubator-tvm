0a87f6bae8099255fd07137ab4dd42e1e7e063b6
Khang D Vu <kvu35@flubber.crnch.gatech.edu>
2020-04-10 01:56:47 -0400

    Draft of VTA device run for vitis driver

--- vta/src/vitis/vitis_driver.cc
+++ vta/src/vitis/vitis_driver.cc
@@ -43,6 +43,8 @@ void VitisProgramBin(cl::Context& context, cl::Device& device, const char *binar
 }*/
 
 
+static cl_int err;
+
 class VTADevice {
   private:
     cl::Context context;
@@ -51,7 +53,6 @@ class VTADevice {
     cl::CommandQueue cmd_queue;
   public:
     VTADevice() {
-      cl_int err;
       auto devices = xcl::get_xil_devices();
       device = devices[0]; // currently fixed with the first device
 
@@ -75,6 +76,7 @@ class VTADevice {
     int Run(vta_phy_addr_t insn_phy_addr,
         uint32_t insn_count,
         uint32_t wait_cycles) {
+      cl::Event event;
       // Data must be alread loaded to host memory
       // over here, just send them & let the kernel function work
       // until it's done
@@ -84,13 +86,24 @@ class VTADevice {
       //for (uint32_t i = 0; i < insn_count; ++i) {
       //  this->Run(insn + i);
       //}
+      //
+      OCL_CHECK(err, cl::Buffer instruction_buff (context,
+              CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+              insn_count * sizeof(VTAGenericInsn),
+              reinterpret_cast<char*>(insn_phy_addr),
+              &err));
+      OCL_CHECK(err, vta = cl::Kernel(program, "vta", &err));
+      OCL_CHECK(err, err = vta.setArg(0, instruction_buff));
+      OCL_CHECK(err, err = cmd_queue.enqueueTask(vta, NULL &event))
+      OCL_CHECK(err, err = q.enqueueMigrateMemObjects([instruction_buff]), CL_MIGRATE_MEM_OBJECT_HOST);
+      q.finish();
       return 0;
     }
 
-  private:
-    void Run(const VTAGenericInsn* insn) {
-      printf("execution...\n");
-    }
+  //private:
+  //  void Run(const VTAGenericInsn* insn) {
+  //    printf("execution...\n");
+  //  }
 };
 
 
@@ -98,23 +111,23 @@ class VTADevice {
  *   vta/include/vta/driver.h
  */
 void* VTAMemAlloc(size_t size, int cached) {
-  //return vta::sim::DRAM::Global()->Alloc(size);
+  return malloc(size);
 }
 
 void VTAMemFree(void* buf) {
-  //vta::sim::DRAM::Global()->Free(buf);
+  free(buf);
 }
 
 vta_phy_addr_t VTAMemGetPhyAddr(void* buf) {
-  //return vta::sim::DRAM::Global()->GetPhyAddr(buf);
+  return vta::sim::DRAM::Global()->GetPhyAddr(buf);
 }
 
 void VTAMemCopyFromHost(void* dst, const void* src, size_t size) {
-  //memcpy(dst, src, size);
+  memcpy(dst, src, size);
 }
 
 void VTAMemCopyToHost(void* dst, const void* src, size_t size) {
-  //memcpy(dst, src, size);
+  memcpy(dst, src, size);
 }
 
 void VTAFlushCache(void* vir_addr, vta_phy_addr_t phy_addr, int size) {
